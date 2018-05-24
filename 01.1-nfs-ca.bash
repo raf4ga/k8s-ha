45bb3c4a9370697aaa4d269b38c7fcfec5ebb521
Alberto Molina Coballes <alb.molina@gmail.com>
2018-05-24 14:15:45 +0200

    Primeros pasos: CA, NFS y etcd

---
+++ 01.1-nfs-ca.bash
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env bash
+
+source ./common.bash
+
+# Install nfs-kernel-server
+
+apt -y install nfs-kernel-server
+
+
+# Export KUBERNETES_CERT_DIR
+cat <<EOF >/etc/exports
+$KUBERNETES_CERT_DIR     $CONTROLLER2_HOSTNAME(ro,sync,no_subtree_check)
+EOF
+
+# Restart NFS service
+
+systemctl restart nfs-server
---
+++ 01.2-nfs-ca.bash
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env bash
+
+source ./common.bash
+
+# Create folder for storing keys, we will use /etc/kubernetes/pki (based on examples like Kubeadm)
+
+mkdir -p "$KUBERNETES_CERT_DIR"
+
+# Install nfs-common package
+
+apt -y install nfs-common
+
+# Import permanently CA files
+
+cat <<EOF >>/etc/fstab
+$CONTROLLER_PRIVATE_IP:$KUBERNETES_CERT_DIR $KUBERNETES_CERT_DIR nfs auto,nofail,noatime 0 0
+EOF
+
+echo "Mounting $KUBERNETES_CERT_DIR"
+
+mount $KUBERNETES_CERT_DIR
